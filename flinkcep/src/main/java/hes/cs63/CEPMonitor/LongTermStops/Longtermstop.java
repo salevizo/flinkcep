ea9bda1f27af48a0b3a21d9a63f76c31c0ceb36c
George Tsiatsios <georget@di.uoa.gr>
2018-12-08 15:49:32 +0200

    fixed patterns

--- flinkcep/src/main/java/hes/cs63/CEPMonitor/LongTermStops/Longtermstop.java
+++ flinkcep/src/main/java/hes/cs63/CEPMonitor/LongTermStops/Longtermstop.java
@@ -18,87 +18,79 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Longtermstop {
 
-    public static Pattern<AisMessage, ?> patternLongStop() {
-        //CHECK PORTS
-        List<String> Ports = new ArrayList<String>();
-        String csvFile ="/home/cer/Desktop/cer/flinkcep/flinkcep/producer/wpi.csv";
+    public  static HashSet <String> listOfPorts=ports();
+
+    public static HashSet<String> ports(){
+        listOfPorts = new HashSet<String>();
+        String csvFile = "/home/cer/Desktop/cer/flinkcep/flinkcep/producer/wpi.csv";
         String line = "";
-
+        String cvsSplitBy = ",";
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
+            String gHash="";
             while ((line = br.readLine()) != null) {
-
-                // use comma as separator
-                String[] coords = line.split(",");
-
-                System.out.println("Coords [lat= " + coords[0] + " , Lon=" + coords[1] + "]");
-                String geoHash1=GeoHash.encodeHash(Double.parseDouble(coords[0]),Double.parseDouble(coords[1]),6);
-                System.out.println(geoHash1);
-                Ports.add(geoHash1);
-
-
+                String[] coordinates = line.split(cvsSplitBy);
+                gHash=GeoHash.encodeHash(Float.valueOf(coordinates[0]),Float.valueOf(coordinates[1]),6);
+                listOfPorts.add(gHash);
             }
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        return listOfPorts;
+    }
 
-        int longterm = 60;
-        Pattern<AisMessage, ?> LongTermStopPattern = Pattern.<AisMessage>begin("stop", AfterMatchSkipStrategy.skipPastLastEvent())
-                //.subtype(AisMessage.class)
+    public static Pattern<AisMessage, ?> patternLongStop() {
+        //CHECK PORTS
+
+
+
+
+        int longterm = 3600;
+        int time_window=7200;
+        Pattern<AisMessage, ?> LongTermStopPattern = Pattern.<AisMessage>begin("start", AfterMatchSkipStrategy.skipPastLastEvent())
+
+                .next("stop")
                 .where(new SimpleCondition<AisMessage>() {
                     @Override
                     public boolean filter(AisMessage event) throws Exception {
-                        boolean near_ports = false;
-                        for(String str: Ports) {
-                            String ship_geohash = GeoHash.encodeHash(event.getLat(),event.getLon(),6);
-                            if(str.equals(ship_geohash))
-                                near_ports = true;
-                            //System.out.printf("Ship near Port\n");
 
-                        }
-                        //System.out.printf("ships: %d %d %f\n", event.getMmsi(), event.getT(),event.getSpeed());
+                        if((event.getSpeed() <1 && listOfPorts.contains(GeoHash.encodeHash(event.getLat(), event.getLon(), 8)) == false)){
+                            
+                            String geoHash1=GeoHash.encodeHash(event.getLat(),event.getLon(),8);
 
-                        if((event.getSpeed() <1 && near_ports == false)){
-                            //low speed away from ports ---> pause
-                           // System.out.printf("Pause by ship %s %d %f\n", event.getMmsi(), event.getT(),event.getSpeed());
-
+                            
 
                             return true;
                         }
                         return false;
                     }
                 })
-                //.oneOrMore()
-                //.timesOrMore(2)
+           
                 .followedBy("stop_ends")
-                //.subtype(AisMessage.class)
+           
                 .where(new IterativeCondition<AisMessage>() {
 
                     @Override
                     public boolean filter(AisMessage event, Context<AisMessage> ctx) throws Exception {
-                        //boolean near_ports = false;
-//                        for(String str: Ports) {
-//                            String ship_geohash = GeoHash.encodeHash(event.getLat(),event.getLon(),6);
-//                            if(str.equals(ship_geohash))
-//                                near_ports = true;
-//                            //System.out.printf("Ship near Port\n");
-//
-//                        }
                         for (AisMessage ev : ctx.getEventsForPattern("stop")) {
-                                String geoHash1=GeoHash.encodeHash(event.getLat(),event.getLon(),6);
                             if ( ev.getMmsi() == event.getMmsi()) {
-                                //System.out.printf("events ship %s %d %s %f?\n",event.getMmsi(),event.getT(),geoHash1,event.getSpeed());
 
-
+                                String geoHash1=GeoHash.encodeHash(event.getLat(),event.getLon(),6);
+                            
                                 String geoHash2=GeoHash.encodeHash(ev.getLat(),ev.getLon(),6);
-                                //System.out.printf("eventszssss ship %s %d %s %f?\n",ev.getMmsi(),ev.getT(),geoHash2,ev.getSpeed());
 
-                                // m conservative events ev.getSpeed() < 2
-                                if((geoHash1.equals(geoHash2)) && (event.getSpeed()<2) && ev.getSpeed()<2  && (Math.abs(event.getT() - ev.getT()) > longterm)){
+                                if((geoHash1.equals(geoHash2)) && (event.getSpeed()<2) && ev.getSpeed()<2  && (Math.abs(event.getT() - ev.getT()) > 1800)){
+                                   System.out.printf("LongStop %s %d %f %s\n?", event.getMmsi(), event.getT(),event.getSpeed(),geoHash1);
+
                                     return true;
                                 }
                                 else{
@@ -111,24 +103,8 @@ public class Longtermstop {
 
                         }
                         return false;
-                    }})
-//                .followedBy("end_of")
-//                .where(new SimpleCondition<AisMessage>() {
-//                    @Override
-//                    public boolean filter(AisMessage aisMessage) throws Exception {
-//                        if(aisMessage.getSpeed()>2){
-//                            String geoHash2=GeoHash.encodeHash(aisMessage.getLat(),aisMessage.getLon(),6);
-//
-//                            System.out.printf("eventszssss ship %s %d %s %f?\n",aisMessage.getMmsi(),aisMessage.getT(),geoHash2,aisMessage.getSpeed());
-//
-//                        }
-//                        else {
-//                            return false;
-//                        }
-//                        return false;
-//                    }
-//                })
-                .within(Time.seconds(200));
+                    }});
+              
         return LongTermStopPattern;
     }
 
@@ -138,7 +114,7 @@ public class Longtermstop {
             public SuspiciousLongStop select(Map<String,List<AisMessage>> pattern) throws Exception {
                 AisMessage stop_vessel = (AisMessage) pattern.get("stop").get(0);
                 AisMessage stop_end_vessel = (AisMessage) pattern.get("stop_ends").get(0);
-              //  AisMessage stop_ends_vessel = (AisMessage) pattern.get("end_of").get(0);
+                //AisMessage stop_ends_vessel = (AisMessage) pattern.get("end_of").get(0);
 
 
                 String geohash= GeoHash.encodeHash(stop_end_vessel.getLat(),stop_end_vessel.getLon(),6);
@@ -151,3 +127,4 @@ public class Longtermstop {
     }
 
 }
+
